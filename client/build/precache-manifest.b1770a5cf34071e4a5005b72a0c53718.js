72568cb46b2c16ab7e6e3193363fb1de70f9fde2
Thomas Cheng <tcheng878@gmail.com>
2020-05-22 11:25:55 -0700

    initial commit2

---
+++ client/build/precache-manifest.b1770a5cf34071e4a5005b72a0c53718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f19fe32e94488cbdad6529578bfa0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c98fe3b5dc831a0e7392",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e86db28d72f7e1249a2a",
+    "url": "/static/js/2.4ad0a82a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ad0a82a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c98fe3b5dc831a0e7392",
+    "url": "/static/js/main.9c746a9c.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
